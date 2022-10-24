187d0b1c016ec69addb3ae10e71a151d972de03f
RooftoproomWhale <gh8238@naver.com>
2022-10-24 22:44:54 +0900

    member-service init

---
+++ backend/member-service/src/test/java/pe/woong/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.woong.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
