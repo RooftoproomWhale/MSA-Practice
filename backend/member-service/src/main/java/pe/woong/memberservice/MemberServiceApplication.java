cb32a4f27b55ed32bd3ed793fd15d028ecc21ec9
RooftoproomWhale <gh8238@naver.com>
2022-11-09 22:42:15 +0900

    add member-service dependency eureka-client

--- backend/member-service/src/main/java/pe/woong/memberservice/MemberServiceApplication.java
+++ backend/member-service/src/main/java/pe/woong/memberservice/MemberServiceApplication.java
@@ -2,8 +2,10 @@ package pe.woong.memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
